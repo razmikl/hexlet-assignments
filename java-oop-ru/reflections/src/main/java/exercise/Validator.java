66668d86d741ba198f241250cbe2fd0865138d80
@hexlet/cli <support@hexlet.io>
2025-02-09 19:21:00 +0300

    submit java-oop-ru/reflections

--- java-oop-ru/reflections/src/main/java/exercise/Validator.java
+++ java-oop-ru/reflections/src/main/java/exercise/Validator.java
@@ -38,10 +38,12 @@ public class Validator {
                 field.setAccessible(true);
                 String value = (String) field.get(obj);
                 MinLength annotation = aClass.getAnnotation(MinLength.class);
+
                 if (field.isAnnotationPresent(NotNull.class) && value == null) {
                     newStringList.add("can not be null");
                     result.put(field.getName(), newStringList);
-                } else {
+                }
+                if (field.isAnnotationPresent(NotNull.class) && value != null) {
                     if (field.isAnnotationPresent(MinLength.class) && annotation.minLength() <= value.length()) {
                         newStringList.add("length less than " + annotation.minLength());
                         result.put(field.getName(), newStringList);
