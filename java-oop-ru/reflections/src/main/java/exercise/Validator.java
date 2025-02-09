8e7c43eac67b0d120493c64c3ec627635892282e
@hexlet/cli <support@hexlet.io>
2025-02-09 18:53:01 +0300

    submit java-oop-ru/reflections

---
+++ java-oop-ru/reflections/src/main/java/exercise/Validator.java
@@ -0,0 +1,59 @@
+package exercise;
+
+import java.lang.reflect.Field;
+// BEGIN
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Validator {
+    public static List<String> validate(Object obj) {
+        var result = new ArrayList<String>();
+
+        try {
+            Class<?> aClass = obj.getClass();
+            Field[] declaredFiels = aClass.getDeclaredFields();
+            for (var field : declaredFiels) {
+                field.setAccessible(true);
+                String value = (String) field.get(obj);
+                if (field.isAnnotationPresent(NotNull.class) && value != null) {
+                    result.add(field.getName());
+                }
+            }
+        } catch (IllegalAccessException e) {
+            e.fillInStackTrace();
+        }
+        return result;
+    }
+
+    public static Map<String, List<String>> advancedValidate(Object obj) {
+        var result = new HashMap<String, List<String>>();
+        var newStringList = new ArrayList<String>();
+
+        try {
+            Class<?> aClass = obj.getClass();
+            Field[] declaredFiels = aClass.getDeclaredFields();
+            for (var field : declaredFiels) {
+                field.setAccessible(true);
+                String value = (String) field.get(obj);
+                MinLength annotation = aClass.getAnnotation(MinLength.class);
+                if (field.isAnnotationPresent(NotNull.class) && value != null) {
+                    newStringList.add("can not be null");
+                    result.put(field.getName(), newStringList);
+                }
+                if (field.isAnnotationPresent(MinLength.class) && annotation.minLength() <= value.length()) {
+                    newStringList.add("length less than " + annotation.minLength());
+                    result.put(field.getName(), newStringList);
+                }
+
+            }
+        } catch (IllegalAccessException e) {
+            e.fillInStackTrace();
+        }
+        return result;
+        }
+    }
+
+
+// END
